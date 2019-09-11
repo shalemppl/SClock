5e519d3aeac6b58800d92d8ef7ac1c320a4a9903
ShalemPPL <shalem.shemtov@gmail.com>
2019-09-11 11:07:02 -0400

    NOT WORKING! Attempting to create a string to run as shell command calling hebcal with date variable

--- clock.py
+++ clock.py
@@ -7,22 +7,27 @@ import os
 #print(datetime.datetime.today(), '\n')
 #print(datetime.datetime.today().weekday())
 
-i = datetime.datetime.today().weekday()
-print(i)
+# Find today's date and create a list of the day, month and year converted to integers
 datelist = str(date.today())
-print(datelist)
+#print(datelist)
 datelist = datelist.split('-')
 for a in range(3):
     datelist[a]=int(datelist[a])
 print(datelist)
-j = 0
 
-if i == 0: j = 4
-elif i == 1: j = 3
-elif i == 2: j = 2
+# Find the next day with a Candle Lighting time by finding today's day of the week and then adding 1 to it until a candle lighting time is found
+i = datetime.datetime.today().weekday()
+#print(i)
+#j = 0
+
+# if i == 0: j = 4
+# elif i == 1: j = 3
+# elif i == 2: j = 2
 #print(j)
 
-cmd = '../hebcal/hebcal -cTO 09 13 2019 > s.txt'
+year = str(datelist[0])
+cmd = '../hebcal/hebcal -cTO ', year, ' > s.txt'
+print(cmd)
 os.system(cmd)
 
 fhandle = open('s.txt')
