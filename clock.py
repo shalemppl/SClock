caba14496a573a4b4476301ef46d6ce99437e534
ShalemPPL <shalem.shemtov@gmail.com>
2019-09-11 11:13:29 -0400

    WORKING! Only prints if there is a Candle Lighting time for today

--- clock.py
+++ clock.py
@@ -13,10 +13,10 @@ datelist = str(date.today())
 datelist = datelist.split('-')
 for a in range(3):
     datelist[a]=int(datelist[a])
-print(datelist)
+#print(datelist)
 
 # Find the next day with a Candle Lighting time by finding today's day of the week and then adding 1 to it until a candle lighting time is found
-i = datetime.datetime.today().weekday()
+#i = datetime.datetime.today().weekday()
 #print(i)
 #j = 0
 
@@ -25,19 +25,25 @@ i = datetime.datetime.today().weekday()
 # elif i == 2: j = 2
 #print(j)
 
+# Create variables based on today's date
 year = str(datelist[0])
-cmd = '../hebcal/hebcal -cTO ', year, ' > s.txt'
-print(cmd)
+month = str(datelist[1])
+day = str(datelist[2])
+
+# Find out if today has a Candle Lighting time
+cmd = '../hebcal/hebcal -cTO ' + month + ' ' + day + ' ' + year + ' > s.txt'
+#print(cmd)
 os.system(cmd)
 
 fhandle = open('s.txt')
 t = []
 for line in fhandle:
     line = line.rstrip()
-    #if line.startswith('Candle'): print(line)
-    t.append(line)
-    print(t)
+    if line.startswith('Candle'): print(line)
+    #t.append(line)
+    #print(t)
 fhandle.close()
+
 #print('Output: ', os.system(cmd))
 #Shabbat = date.today() + j
 #print('Shabbat will start on Friday, ', Shabbat)
